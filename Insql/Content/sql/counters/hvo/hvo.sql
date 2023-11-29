285cc3309c405a5b84ce3d4c6dafb414fdfaa94b
yura.dulin <yura.dulin@gmail.com>
2023-11-29 14:00:08 +0300

    [Edit] Изменены подписи

--- Insql/Content/sql/counters/hvo/hvo.sql
+++ Insql/Content/sql/counters/hvo/hvo.sql
@@ -15,8 +15,8 @@ SELECT
 	CAST(isnull(H02_VB11g001,0)*isnull(H02_SY00A700,0) as decimal(15,2)) as "G сырой воды на осв-ль №2, т/ч [H02_VB11g001]",
 	CAST(isnull(H02_VA22F001,0)*isnull(H02_SY00A700,0) as decimal(15,2)) as "G ХОВ ХВО II, т/ч [H02_VA22F001]",
 	CAST(isnull(H02_VB12F001,0)*isnull(H02_SY00A700,0) as decimal(15,2)) as "G сырой воды на осв-ль №1, т/ч [H02_VB12F001]",
-	CAST(isnull(H02_KK11_F001,0)*isnull(H02_SY00A700,0) as decimal(15,2)) as "G к-та в бак декарб.воды, т/ч [H02_KK11_F001]",
-	Round((Isnull(H02_VB11G001, 0) + H02_VB12F001) - H01_VA21F001 * 1.28 - U04_UM41F001 * 0.97 * 1.12, 2) as "Собственные нужды, т/ч [Hint:(Isnull(H02_VB11G001, 0) + H02_VB12F001) - H01_VA21F001 * 1.28 - U04_UM41F001 * 0.97 * 1.12]"
+	CAST(isnull(H02_KK11_F001,0)*isnull(H02_SY00A700,0) as decimal(15,2)) as "G к-та в бак<br>декарб.воды, т/ч [H02_KK11_F001]",
+	Round((Isnull(H02_VB11G001, 0) + H02_VB12F001) - H01_VA21F001 * 1.28 - U04_UM41F001 * 0.97 * 1.12, 2) as "Собст-венные нужды, т/ч [Hint:(Isnull(H02_VB11G001, 0) + H02_VB12F001) - H01_VA21F001 * 1.28 - U04_UM41F001 * 0.97 * 1.12]"
 FROM
 	OpenQuery(INSQL,
 	"SELECT DateTime,
@@ -50,16 +50,16 @@ SET QUOTED_IDENTIFIER OFF
 SELECT
 	DateAdd(mi, -5, DateTime) as "Дата",
 	null,
-	CAST(isnull(H01_KB21F001_H1,0) as decimal(15,2)) as "G загр. конд. из КТЦ за п/ч, т/ч [H01_KB21F001_H1]",
-	CAST(isnull(H01_KC21F001_H1,0) as decimal(15,2)) as "G очищ. конд. в КТЦ за п/ч, т/ч [H01_KC21F001_H1]",
-	CAST(isnull(H01_VA21F001_H1,0) as decimal(15,2)) as "G ХОВ за п/ч, т/ч [H01_VA21F001_H1]",
-	CAST(isnull(H02_KB11F001_H1,0) as decimal(15,2)) as "G загр. конд. до ф-ров КО за п/ч, т/ч [H02_KB11F001_H1]",
-	CAST(isnull(H01_KC22F001_H1,0) as decimal(15,2)) as "G очищ. конд.после ф-ров КО за п/ч, т/ч [H01_KC22F001_H1]",
-	CAST(isnull(H02_VB11F001_H1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №2 за п/ч, т/ч [H02_VB11F001_H1]",
-	CAST(isnull(H02_VA22F001_H1,0) as decimal(15,2)) as "G ХОВ ХВО II за п/ч, т/ч [H02_VA22F001_H1]",
-	CAST(isnull(H02_VB12F001_H1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №1 за п/ч, т/ч [H02_VB12F001_H1]",
-	CAST(isnull(H02_KK11_F001_H1,0) as decimal(15,2)) as "G к-та в бак декарб.воды за п/ч, т/ч [H02_KK11_F001_H1]",
-	Round(U04_UM41F001_H1, 2) as "Масса ПОДПИТКИ за прошедший час (4-210т3н01) [U04_UM41F001_H1]",
+	CAST(isnull(H01_KB21F001_H1,0) as decimal(15,2)) as "G загр. конд. из КТЦ, т/ч [H01_KB21F001_H1]",
+	CAST(isnull(H01_KC21F001_H1,0) as decimal(15,2)) as "G очищ. конд. в КТЦ, т/ч [H01_KC21F001_H1]",
+	CAST(isnull(H01_VA21F001_H1,0) as decimal(15,2)) as "G ХОВ, т/ч [H01_VA21F001_H1]",
+	CAST(isnull(H02_KB11F001_H1,0) as decimal(15,2)) as "G загр. конд. до ф-ров КО, т/ч [H02_KB11F001_H1]",
+	CAST(isnull(H01_KC22F001_H1,0) as decimal(15,2)) as "G очищ. конд.после ф-ров КО, т/ч [H01_KC22F001_H1]",
+	CAST(isnull(H02_VB11F001_H1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №2, т/ч [H02_VB11F001_H1]",
+	CAST(isnull(H02_VA22F001_H1,0) as decimal(15,2)) as "G ХОВ ХВО II, т/ч [H02_VA22F001_H1]",
+	CAST(isnull(H02_VB12F001_H1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №1, т/ч [H02_VB12F001_H1]",
+	CAST(isnull(H02_KK11_F001_H1,0) as decimal(15,2)) as "G к-та в бак<br>декарб.воды, т/ч [H02_KK11_F001_H1]",
+	Round(U04_UM41F001_H1, 2) as "G подпитки [U04_UM41F001_H1]",
 	Round((Isnull(Cast(H02_VB11F001_H1 as decimal(15,6)), 0) + H02_VB12F001_H1) - H01_VA21F001_H1 * 1.28 - U04_UM41F001_H1 * 0.97 * 1.12, 2) as "Собственные нужды, т/ч"
 FROM
 	OpenQuery(INSQL,
@@ -93,16 +93,16 @@ SET QUOTED_IDENTIFIER OFF
 SELECT
 	DateAdd(mi, -1445, DateTime) as "Дата",
 	null,
-	CAST(isnull(H01_KB21F001_S1,0) as decimal(15,2)) as "G загр. конд. из КТЦ за п/с, т/сут [H01_KB21F001_S1]",
-	CAST(isnull(H01_KC21F001_S1,0) as decimal(15,2)) as "G очищ. конд. в КТЦ за п/с, т/сут [H01_KC21F001_S1]",
-	CAST(isnull(H01_VA21F001_S1,0) as decimal(15,2)) as "G ХОВ за п/с, т/сут) [H01_VA21F001_S1]",
-	CAST(isnull(H02_KB11F001_S1,0) as decimal(15,2)) as "G загр. конд. до ф-ров КО за п/с, т/сут [H02_KB11F001_S1]",
-	CAST(isnull(H01_KC22F001_S1,0) as decimal(15,2)) as "G очищ. конд.после ф-ров КО за п/с, т/сут [H01_KC22F001_S1]",
-	CAST(isnull(H02_VB11F001_S1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №2 за п/с, т/сут [H02_VB11F001_S1]",
-	CAST(isnull(H02_VA22F001_S1,0) as decimal(15,2)) as "G ХОВ ХВО II за п/с, т/сут [H02_VA22F001_S1]",
-	CAST(isnull(H02_VB12F001_S1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №1 за п/с, т/сут [H02_VB12F001_S1]",
-	CAST(isnull(H02_KK11_F001_S1,0) as decimal(15,2)) as "G к-та в бак декарб.воды за п/с, т/сут [H02_KK11_F001_S1]",
-	Round(U04_UM41F001_S1, 2) as "Масса Подпитки за прошедшие сутки (4-3-211-01) [U04_UM41F001_S1]",
+	CAST(isnull(H01_KB21F001_S1,0) as decimal(15,2)) as "G загр. конд. из КТЦ, т/сут [H01_KB21F001_S1]",
+	CAST(isnull(H01_KC21F001_S1,0) as decimal(15,2)) as "G очищ. конд. в КТЦ, т/сут [H01_KC21F001_S1]",
+	CAST(isnull(H01_VA21F001_S1,0) as decimal(15,2)) as "G ХОВ, т/сут [H01_VA21F001_S1]",
+	CAST(isnull(H02_KB11F001_S1,0) as decimal(15,2)) as "G загр. конд. до ф-ров КО, т/сут [H02_KB11F001_S1]",
+	CAST(isnull(H01_KC22F001_S1,0) as decimal(15,2)) as "G очищ. конд.после ф-ров КО, т/сут [H01_KC22F001_S1]",
+	CAST(isnull(H02_VB11F001_S1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №2, т/сут [H02_VB11F001_S1]",
+	CAST(isnull(H02_VA22F001_S1,0) as decimal(15,2)) as "G ХОВ ХВО II, т/сут [H02_VA22F001_S1]",
+	CAST(isnull(H02_VB12F001_S1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №1, т/сут [H02_VB12F001_S1]",
+	CAST(isnull(H02_KK11_F001_S1,0) as decimal(15,2)) as "G к-та в бак<br>декарб.воды, т/сут [H02_KK11_F001_S1]",
+	Round(U04_UM41F001_S1, 2) as "G подпитки [U04_UM41F001_S1]",
 	Round((Isnull(H02_VB11F001_S1, 0) + H02_VB12F001_S1) - H01_VA21F001_S1 * 1.28 - U04_UM41F001_S1 * 0.97 * 1.12, 2) as "Собственные нужды, т/ч"
 FROM
 	OpenQuery(INSQL, 
@@ -137,14 +137,14 @@ SELECT
 	DateAdd(mi, -5, DateTime) as "Дата",
 	null,
 	CHAR(192+isnull(CLC_ShiftDuty,0)) as "Cме- на [CLC_ShiftDuty]",
-	CAST(isnull(H01_KB21F001_H1,0) as decimal(15,2)) as "G загр. конд. из КТЦ за п/см, т/см [H01_KB21F001_H1]",
-	CAST(isnull(H01_KC21F001_H1,0) as decimal(15,2)) as "G очищ. конд. в КТЦ за п/см, т/см [H01_KC21F001_H1]",
-	CAST(isnull(H01_VA21F001_H1,0) as decimal(15,2)) as "G ХОВ за п/см, т/см [H01_VA21F001_H1]",
-	CAST(isnull(H02_KB11F001_H1,0) as decimal(15,2)) as "G загр. конд. до ф-ров КО за п/см, т/см [H02_KB11F001_H1]",
-	CAST(isnull(H01_KC22F001_H1,0) as decimal(15,2)) as "G очищ. конд.после ф-ров КО за п/см, т/см [H01_KC22F001_H1]",
-	CAST(isnull(H02_VB11F001_H1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №2 за п/см, т/см [H02_VB11F001_H1]",
-	CAST(isnull(H02_VA22F001_H1,0) as decimal(15,2)) as "G ХОВ ХВО II за п/см, т/см [H02_VA22F001_H1]",
-	CAST(isnull(H02_VB12F001_H1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №1 за п/см, т/см [H02_VB12F001_H1]"
+	CAST(isnull(H01_KB21F001_H1,0) as decimal(15,2)) as "G загр. конд. из КТЦм, т/см [H01_KB21F001_H1]",
+	CAST(isnull(H01_KC21F001_H1,0) as decimal(15,2)) as "G очищ. конд. в КТЦм, т/см [H01_KC21F001_H1]",
+	CAST(isnull(H01_VA21F001_H1,0) as decimal(15,2)) as "G ХОВм, т/см [H01_VA21F001_H1]",
+	CAST(isnull(H02_KB11F001_H1,0) as decimal(15,2)) as "G загр. конд. до ф-ров КОм, т/см [H02_KB11F001_H1]",
+	CAST(isnull(H01_KC22F001_H1,0) as decimal(15,2)) as "G очищ. конд.после ф-ров КОм, т/см [H01_KC22F001_H1]",
+	CAST(isnull(H02_VB11F001_H1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №2м, т/см [H02_VB11F001_H1]",
+	CAST(isnull(H02_VA22F001_H1,0) as decimal(15,2)) as "G ХОВ ХВО IIм, т/см [H02_VA22F001_H1]",
+	CAST(isnull(H02_VB12F001_H1,0) as decimal(15,2)) as "G сыр.воды на осв-ль №1м, т/см [H02_VB12F001_H1]"
 FROM
 	OpenQuery(INSQL, 
 	"SELECT DateTime,  H02_VA22F001_H1, H02_VB12F001_H1, H01_KB21F001_H1, H01_KC21F001_H1, H02_VB11F001_H1, CLC_ShiftDuty, H01_VA21F001_H1, H02_KB11F001_H1, H01_KC22F001_H1 FROM Runtime.dbo.AnalogWideHistory 
