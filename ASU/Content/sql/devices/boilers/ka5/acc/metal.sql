19e49ddb089af384f2eb8afdcd1c0a5eee7e4b15
LunaryCrow <tapahtyji@inbox.ru>
2024-04-15 14:03:01 +0300

    Шапка таблицы заменена на компактную, по аналогии с К/а № 3. Старая полная шапка закомментирована. ОТЧЕТЫ ПО ОБОРУДОВАНИЮ / КОТЛОАГРЕГАТЫ / К/а № 5 / Суточные ведомости / Контроль металла

--- ASU/Content/sql/devices/boilers/ka5/acc/metal.sql
+++ ASU/Content/sql/devices/boilers/ka5/acc/metal.sql
@@ -9,22 +9,42 @@ SET QUOTED_IDENTIFIER OFF
 SELECT
 	DateTime as "Дата",
 	null,
-	ROUND(KA5_VG1_K052, 2) as "[KA5_VG1_K052][Description]",
-	ROUND(KA5_VG1_K094, 2) as "[KA5_VG1_K094][Description]",
-	ROUND(KA5_VG1_K053, 2) as "[KA5_VG1_K053][Description]",
-	ROUND(KA5_VG1_K098, 2) as "[KA5_VG1_K098][Description]",
-	ROUND(KA5_VG1_K054, 2) as "[KA5_VG1_K054][Description]",
-	ROUND(KA5_VG1_K113, 2) as "[KA5_VG1_K113][Description]",
-	ROUND(KA5_VG1_K055, 2) as "[KA5_VG1_K055][Description]",
-	ROUND(KA5_VG1_K114, 2) as "[KA5_VG1_K114][Description]",
-	ROUND(KA5_VG1_K056, 2) as "[KA5_VG1_K056][Description]",
-	ROUND(KA5_VG1_K115, 2) as "[KA5_VG1_K115][Description]",
-	ROUND(KA5_VG1_K057, 2) as "[KA5_VG1_K057][Description]",
-	ROUND(KA5_VG1_K116, 2) as "[KA5_VG1_K116][Description]",
-	ROUND(KA5_VG1_K048, 2) as "[KA5_VG1_K048][Description]",
-	ROUND(KA5_VG1_K049, 2) as "[KA5_VG1_K049][Description]",
-	ROUND(KA5_VG1_K050, 2) as "[KA5_VG1_K050][Description]",
-	ROUND(KA5_VG1_K051, 2) as "[KA5_VG1_K051][Description]"
+	ROUND(KA5_VG1_K052, 2) as "1, °С [KA5_VG1_K052]",
+	ROUND(KA5_VG1_K094, 2) as "2, °С [KA5_VG1_K094]",
+	ROUND(KA5_VG1_K053, 2) as "3, °С [KA5_VG1_K053]",
+	ROUND(KA5_VG1_K098, 2) as "4, °С [KA5_VG1_K098]",
+	ROUND(KA5_VG1_K054, 2) as "5, °С [KA5_VG1_K054]",
+	ROUND(KA5_VG1_K113, 2) as "6, °С [KA5_VG1_K113]",
+	ROUND(KA5_VG1_K055, 2) as "7, °С [KA5_VG1_K055]",
+	ROUND(KA5_VG1_K114, 2) as "8, °С [KA5_VG1_K114]",
+	ROUND(KA5_VG1_K056, 2) as "9, °С [KA5_VG1_K056]",
+	ROUND(KA5_VG1_K115, 2) as "10, °С [KA5_VG1_K115]",
+	ROUND(KA5_VG1_K057, 2) as "11, °С [KA5_VG1_K057]",
+	ROUND(KA5_VG1_K116, 2) as "12, °С [KA5_VG1_K116]",
+	null,
+	ROUND(KA5_VG1_K048, 2) as "№6, °С [KA5_VG1_K048]",
+	ROUND(KA5_VG1_K049, 2) as "№12, °С [KA5_VG1_K049]",
+	ROUND(KA5_VG1_K050, 2) as "№66, °С [KA5_VG1_K050]",
+	ROUND(KA5_VG1_K051, 2) as "№72, °С [KA5_VG1_K051]"
+
+	/* старое, полные названия столбцов */
+	/*ROUND(KA5_VG1_K052, 2) as "[KA5_VG1_K052][Description]",*/
+	/*ROUND(KA5_VG1_K094, 2) as "[KA5_VG1_K094][Description]",*/
+	/*ROUND(KA5_VG1_K053, 2) as "[KA5_VG1_K053][Description]",*/
+	/*ROUND(KA5_VG1_K098, 2) as "[KA5_VG1_K098][Description]",*/
+	/*ROUND(KA5_VG1_K054, 2) as "[KA5_VG1_K054][Description]",*/
+	/*ROUND(KA5_VG1_K113, 2) as "[KA5_VG1_K113][Description]",*/
+	/*ROUND(KA5_VG1_K055, 2) as "[KA5_VG1_K055][Description]",*/
+	/*ROUND(KA5_VG1_K114, 2) as "[KA5_VG1_K114][Description]",*/
+	/*ROUND(KA5_VG1_K056, 2) as "[KA5_VG1_K056][Description]",*/
+	/*ROUND(KA5_VG1_K115, 2) as "[KA5_VG1_K115][Description]",*/
+	/*ROUND(KA5_VG1_K057, 2) as "[KA5_VG1_K057][Description]",*/
+	/*ROUND(KA5_VG1_K116, 2) as "[KA5_VG1_K116][Description]",*/
+	/*ROUND(KA5_VG1_K048, 2) as "[KA5_VG1_K048][Description]",*/
+	/*ROUND(KA5_VG1_K049, 2) as "[KA5_VG1_K049][Description]",*/
+	/*ROUND(KA5_VG1_K050, 2) as "[KA5_VG1_K050][Description]",*/
+	/*ROUND(KA5_VG1_K051, 2) as "[KA5_VG1_K051][Description]"*/
+	
 FROM
 	OpenQuery(INSQL,
 	"SELECT DateTime,
